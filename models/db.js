9d26c12505115ac05276a5a65deb7dc05d7de186
Quinlan <qnhampton93@gmail.com>
2019-10-05 13:07:15 -0700

    fixed .eslintrc.json to allow es6 and prettified db file

--- models/db.js
+++ models/db.js
@@ -1,17 +1,22 @@
-'use strict'
+"use strict";
 
-const Sequelize = require('sequelize');
-const env = require('./env');
-const sequelize = new Sequelize(env.DATABASE_NAME, env.DATABASE_USERNAME, env.DATABASE_PASSWORD, {
+const Sequelize = require("sequelize");
+const env = require("./env");
+const sequelize = new Sequelize(
+  env.DATABASE_NAME,
+  env.DATABASE_USERNAME,
+  env.DATABASE_PASSWORD,
+  {
     host: env.DATABASE_HOST,
     port: env.DATABASE_PORT,
     dialect: env.DATABASE_DIALECT,
     define: {
-        underscored: true
+      underscored: true
     }
-});
+  }
+);
 
-// Connect all the models/tables in the database to a db object, 
+// Connect all the models/tables in the database to a db object,
 //so everything is accessible via one object
 const db = {};
 
@@ -19,11 +24,11 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 //Models/tables
-db.users = require('../models/users.js')(sequelize, Sequelize);
-db.categories = require('../models/categories.js')(sequelize, Sequelize);
-db.profiles = require('../models/profieles.js')(sequelize, Sequelize);
-db.matches = require('../models/matches.js')(sequelize, Sequelize);
-db.chats = require('../models/chats.js')(sequelize, Sequelize);
+db.users = require("../models/users.js")(sequelize, Sequelize);
+db.categories = require("../models/categories.js")(sequelize, Sequelize);
+db.profiles = require("../models/profieles.js")(sequelize, Sequelize);
+db.matches = require("../models/matches.js")(sequelize, Sequelize);
+db.chats = require("../models/chats.js")(sequelize, Sequelize);
 
 //Relations
 db.User.hasMany(db.Profiles);
@@ -34,7 +39,5 @@ db.Profiles.belongsTo(db.User);
 db.Matches.hasOne(db.Chat);
 db.Matches.belongsTo(db.Profiles);
 db.Chats.belongsTo(db.Matches);
-
-
 
 module.exports = db;
