8326a34c79c8881103262cc46d2409bbf57ec762
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-10-04 22:22:28 -0700

    please Travis, let me uploadgit add .

--- public/js/zipRadius.js
+++ public/js/zipRadius.js
@@ -1,54 +1,57 @@
 function getLangLat(USERZIP) {
-    // document.write("Loading");
-    var api = "https://api.promaptools.com/service/us/zip-lat-lng/get/?zip="+USERZIP+"&key=17o8dysaCDrgv1c";
-    $.ajax({
-        url: api,
-        type: 'GET'
-    }).then(function(result) {
-        console.log(result.output[0].latitude);
-        console.log(result.output[0].longitude);
-        ftnLoadPostcodes(10, result.output[0].latitude, result.output[0].longitude);
-    });
+  var api =
+    "https://api.promaptools.com/service/us/zip-lat-lng/get/?zip=" +
+    USERZIP +
+    "&key=17o8dysaCDrgv1c";
+  $.ajax({
+    url: api,
+    type: "GET"
+  }).then(function(result) {
+    console.log(result.output[0].latitude);
+    console.log(result.output[0].longitude);
+    ftnLoadPostcodes(10, result.output[0].latitude, result.output[0].longitude);
+  });
 }
 
 function ftnLoadPostcodes(givenRad, lat, lng) {
-    var rn = Math.floor(Math.random() * 9999);
-    var ajaxurl = "https://cors-anywhere.herokuapp.com/https://www.freemaptools.com/ajax/us/get-all-zip-codes-inside.php";
-    $.ajax({
-        url: ajaxurl,
-        type: "GET",
-        data: {
-            "radius": givenRad,
-            "lat": lat,
-            "lng": lng,
-            "rn": rn,
-            "showPOboxes": false
-        },
-        dataType: 'xml',
-        success: function (result) {
-            console.log(result);
-            sendZip(result);
-        },
-        error: function (x, y, z) {
-            console.log(y);
-        }
-    });
+  var rn = Math.floor(Math.random() * 9999);
+  var ajaxurl =
+    "https://cors-anywhere.herokuapp.com/https://www.freemaptools.com/ajax/us/get-all-zip-codes-inside.php";
+  $.ajax({
+    url: ajaxurl,
+    type: "GET",
+    data: {
+      radius: givenRad,
+      lat: lat,
+      lng: lng,
+      rn: rn,
+      showPOboxes: false
+    },
+    dataType: "xml",
+    success: function(result) {
+      console.log(result);
+      sendZip(result);
+    },
+    error: function(x, y, z) {
+      console.log(x, y, z);
+    }
+  });
 }
 
 function sendZip(result) {
-    var zipList = [];
-    var xmarkers = result.documentElement.getElementsByTagName("postcode");
-    console.log(xmarkers);
-    for (var i = 0; i < xmarkers.length; i++) {
-        zipList.push({ zip: xmarkers[i].getAttribute("postcode")});
-    }
-    console.log(zipList);
-    $.ajax({
-        url: "/testing",
-        type: 'POST',
-        data: zipList
-    });
-    console.log("DONE!");
+  var zipList = [];
+  var xmarkers = result.documentElement.getElementsByTagName("postcode");
+  console.log(xmarkers);
+  for (var i = 0; i < xmarkers.length; i++) {
+    zipList.push({ zip: xmarkers[i].getAttribute("postcode") });
+  }
+  console.log(zipList);
+  $.ajax({
+    url: "/testing",
+    type: "POST",
+    data: zipList
+  });
+  console.log("DONE!");
 }
 
 getLangLat(92111);
