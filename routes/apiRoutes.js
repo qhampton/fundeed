ce7ede0a6b9348d2f014798c55b38715f82f8d30
Andres Ruiz-Velediaz <aruiz858@gmail.com>
2019-10-08 20:53:48 -0700

    Fixed some auth related things in server and app

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 module.exports = function(app) {
   // Get all examples
   app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+    db.User.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
       console.log("log");
     });
@@ -11,14 +11,14 @@ module.exports = function(app) {
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+    db.User.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+    db.User.destroy({ where: { id: req.params.id } }).then(function(
       dbExample
     ) {
       res.json(dbExample);
