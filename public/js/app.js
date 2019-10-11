5cd23b2647d83882a0b0696ffc6a8a3be4aeb5d6
Max <mhandler1991@gmail.com>
2019-10-10 20:26:42 -0700

    additional logging and framework for finding matches and profiles and determining how to procceed

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,11 @@
+/* eslint-disable padded-blocks */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable indent */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-unused-vars */
+
+
+console.log(
 //* ***************************************************************
 //
 //                        ---                                     
@@ -24,39 +30,103 @@
 //
 //
 //* ***************************************************************
+);
+
 
 // Run when Document is Ready
 $(document).ready(function() {
-  // *******************
-  // Global Variables
-  // *******************
 
-  // *******************
-  // User Swipe Functionality
-  // *******************
+    console.log("Document is Ready...");
+    
+    // *******************
+    // Global Variables
+    // *******************
 
-  // On User Click to Accept (Connect)
-  $('#connectBttn').on("click", function() {
-    // Grab User-Id's
-    var currentUser = "";
-    var connectedUser = "";
 
-    // Check Server to see if there is a Current MATCH record
 
-    // If there is a MATCH record - Update Match Record
+    // *******************
+    // User Swipe Functionality
+    // *******************
 
-    // If there is NOT a Match record - Create a Match Record
+    // On User Click to Accept (Connect)
+    $('#connectBttn').on("click", function() {
+    
+        // Grab User-Id's
+        var currentUser = ""; // Logged in User
+        var connectedUser = ""; // The OTHER User who was matched with
 
-    // Then Display NEXT Potential MATCH
+        // Check Server to see if there is a Current MATCH record
+        app.get("/api/matches", function(req, res) {
+
+            console.log("attemptign to find match record...");
+
+            db.User.findAll({}).then(function(matches) {
+                res.json(matches);
+                console.log(matches);
+            });
+
+            // Determine if there are any MATCH Records
+            if (matches === 0) {
+                
+                // If there is a MATCH record - Update Match Record
+                console.log("Updating Match Record...");
+                
+                app.put("/api/matches/:id", secured(), function(req, res) {
+                    console.log("Updating Match Record...");
+
+                });
+
+            } else {
+
+                // If there is NOT a Match record - Create a Match Record
+                console.log("Creating a new Match Record...");
+
+                app.post("/api/matches", secured(), function(req, res) {
+                    console.log("POSTING NEW Match Record ");
+
+                  });
+            }
+        });
+            
+        // Then GET NEXT PROFILE
+        app.get("/api/examples", function(req, res) {
+
+            console.log("Getting Next Match...")
+
+            db.User.findAll({}).then(function(profile) {
+            
+                console.log(profile);
+
+              res.json(profile);
+
+            });
+        });
+        
+        // Then Display NEXT Potential MATCH -- Update Page with new Profile Info
+
+
+
   });
 
   // On User Click to Accept (Connect)
   $('#passBttn').on("click", function() {
-    // Grab User-Id's
-    var currentUser = "";
-    var connectedUser = "";
 
-    // Then Display NEXT Potential MATCH
+        // Then GET NEXT PROFILE
+        app.get("/api/examples", function(req, res) {
+
+            console.log("Getting Next Match...")
+
+            db.User.findAll({}).then(function(profile) {
+            
+                console.log(profile);
+
+              res.json(profile);
+
+            });
+        });
+        
+        // Then Display NEXT Potential MATCH -- Update Page with new Profile Info
+
   });
 // END
 });
