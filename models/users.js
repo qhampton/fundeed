490719b141acd312f0799cdeb76f419ccb3d14fc
Quinlan <qnhampton93@gmail.com>
2019-10-11 15:47:51 -0700

    merging

--- models/users.js
+++ models/users.js
@@ -60,5 +60,18 @@ module.exports = function(sequelize, DataTypes) {
       defaultValue: "active"
     }
   });
+  User.associate = function(models) {
+    // Associating User with Posts
+    // When an User is deleted, also delete any associated Posts
+    User.hasMany(models.Post, {
+      onDelete: "cascade"
+    });
+  };
+
+  User.associate = function(models) {
+    User.hasMany(models.Profiles, {
+      onDelete: "cascade"
+    });
+  };
   return User;
 };
