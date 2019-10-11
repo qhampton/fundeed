2779a0cfcb90a0e99c98960dad1447c8c578d5c0
Max <mhandler1991@gmail.com>
2019-10-10 20:01:18 -0700

    client side js framework

---
+++ public/js/app.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-unused-vars */
+//* ***************************************************************
+//
+//                        ---                                     
+//                     -        --                             
+//                 --( /     \ )XXXXXXXXXXXXX                   
+//             --XXX(   O   O  )XXXXXXXXXXXXXXX-              
+//            /XXX(       U     )        XXXXXXX\               
+//          /XXXXX(              )--   XXXXXXXXXXX\             
+//         /XXXXX/ (      O     )   XXXXXX   \XXXXX\
+//         XXXXX/   /            XXXXXX   \   \XXXXX----        
+//         XXXXXX  /          XXXXXX         \  ----  -         
+// ---     XXX  /          XXXXXX      \           ---        
+//   --  --  /      /\  XXXXXX            /     ---=         
+//     -        /    XXXXXX              '--- XXXXXX         
+//       --\/XXX\ XXXXXX                      /XXXXX         
+//         \XXXXXXXXX                        /XXXXX/
+//          \XXXXXX                         /XXXXX/         
+//            \XXXXX--  /                -- XXXX/       
+//             --XXXXXXX---------------  XXXXX--         
+//                \XXXXXXXXXXXXXXXXXXXXXXXX-            
+//                  --XXXXXXXXXXXXXXXXXX-
+//
+//
+//* ***************************************************************
+
+// Run when Document is Ready
+$(document).ready(function () {
+
+  // *******************
+  // Global Variables
+  // *******************
+
+  // *******************
+  // User Swipe Functionality
+  // *******************
+
+  // On User Click to Accept (Connect)
+  $('#connectBttn').on("click", function() {
+    // Grab User-Id's
+    var currentUser = "";
+    var connectedUser = "";
+
+    // Check Server to see if there is a Current MATCH record
+
+    // If there is a MATCH record - Update Match Record
+
+    // If there is NOT a Match record - Create a Match Record
+
+    // Then Display NEXT Potential MATCH
+  });
+
+  // On User Click to Accept (Connect)
+  $('#passBttn').on("click", function() {
+    // Grab User-Id's
+    var currentUser = "";
+    var connectedUser = "";
+
+    // Then Display NEXT Potential MATCH
+  });
+// END
+});
