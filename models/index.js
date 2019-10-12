ada1235beea85df50cfa84242128b32283e2c9d9
Quinlan <qnhampton93@gmail.com>
2019-10-11 21:52:28 -0700

    should have stayed in my lane - simplified models

--- models/index.js
+++ models/index.js
@@ -39,17 +39,4 @@ Object.keys(db).forEach(function(modelName) {
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
-//Models/tables
-db.users = require("../models/users.js")(sequelize, Sequelize);
-// db.categories = require("../models/categories.js")(sequelize, Sequelize);
-db.profiles = require("../models/profiles.js")(sequelize, Sequelize);
-db.matches = require("../models/matches.js")(sequelize, Sequelize);
-db.chats = require("../models/chats.js")(sequelize, Sequelize);
-//Relations
-// db.User.hasMany(db.Profiles, {foreignKey: 'auth_id'});
-// db.Profiles.hasMany(db.Matches, {foreignKey: 'profileID'});
-// db.Profiles.belongsTo(db.User, {foreignKey: 'auth_id'});
-// db.Matches.hasOne(db.Chats, {foreignKey: 'matchID'});
-// db.Matches.belongsTo(db.Profiles, {foreignKey: 'profileID'});
-// db.Chats.belongsTo(db.Matches, {foreignKey: 'matchID'});
 module.exports = db;
