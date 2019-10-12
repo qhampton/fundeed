7863d660f4b1ef3ab0c08cbc3e1949d84a336964
Quinlan <qnhampton93@gmail.com>
2019-10-12 00:11:35 -0700

    fixed column name and created dummy data for all tables

--- models/seeds.sql
+++ models/seeds.sql
@@ -1,21 +1,35 @@
-USE project2DB;
+DROP DATABASE IF EXISTS project2DB;
+CREATE DATABASE project2DB;
 
-INSERT INTO users( username, auth_id, lastName, firstName, birthdate, zipcode, searchRadius, ziplist, email, lastLogin, status, createdAt, updatedAt)
-VALUES('sam@fuckyou.com', 'auth0|5d98fdc1242475025b4c4095', 'Ta', 'Max', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
-,('bill@fuckyou.com', 'auth0|5d98fdc12424753c5b4c4095', 'To', 'Brandon', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
-,('max@fuckyou.com', 'auth0|5d98fdc1242475045b4c4095', 'Ruiz', 'Bob', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
-,('quin@fuckyou.com', 'auth0|5d98fdc12424755c5b4c4095', 'Velediaz', 'Lily', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
-,('andres@fuckyou.com', 'auth0|5d98fdc12424650c5b4c4095', 'Torres', 'Jorge', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
-,('brandon@fuckyou.com', 'auth0|5d98fdc12427750c5b4c4095', 'Rodriguez', 'Will', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
-,('louis@fuckyou.com', 'auth0|5d98fdc12424780c5b4c4095', 'Nguyen', 'Tristan', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
-,('harrison@fuckyou.com', 'auth0|5d98fdc12494750c5b4c4095', 'Pham', 'Yugi', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
-,('matt@fuckyou.com', 'auth0|5d98fdc12424758c5b4c4095', 'Xi', 'Seto', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
-,('raul@fuckyou.com', 'auth0|5d98fdc12424710c5b4c4095', 'Smith', 'John', '1990-01-01', '92020', '5', NULL, 'bob@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43');
+DROP DATABASE IF EXISTS testdb;
+CREATE DATABASE testdb;
 
 USE project2DB;
 
-INSERT INTO profiles(categoryType, about, createdAt, updatedAt, UserAuthID)
-VALUES('Food', 'I am hungry!', '2019-10-12 00:02:44', '2019-10-12 02:39:43','auth0|5d98fdc1242475025b4c4095'),
-('Music', 'I play the saxophone', '2019-10-12 00:02:44', '2019-10-12 02:39:43', 'auth0|5d98fdc1242475025b4c4095'),
-('Food', 'Cheese -cheese - and more cheese', '2019-10-12 00:02:44', '2019-10-12 02:39:43','auth0|5d98fdc12424780c5b4c4095'),
-('Food', 'Tacos are life.', '2019-10-12 00:02:44', '2019-10-12 02:39:43','auth0|5d98fdc12424755c5b4c4095');
+INSERT INTO users( username, auth_id, lastName, firstName, birthdate, zipcode, searchRadius, ziplist, bio,  email, lastLogin, status, createdAt, updatedAt)
+VALUES('sam@fuckyou.com', 'auth0|5d98fdc1242475025b4c4095', 'Ta', 'Max', '1990-01-01', '92020', '5', NULL,'I know HTML, CSS, and React. I am looking to create an application to cure my lack of personality. Any python coders out there?', 'sam@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
+,('bill@fuckyou.com', 'auth0|5d98fdc12424753c5b4c4095', 'To', 'Brandon', '1990-01-01', '92020', '5', NULL,'I do not know how to cook at all. Looking to learn how to cook fresh pasta.', 'bill@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
+,('max@fuckyou.com', 'auth0|5d98fdc1242475045b4c4095', 'Ruiz', 'Bob', '1990-01-01', '92020', '5', NULL,'I love creating computers from scratch', 'max@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
+,('quin@fuckyou.com', 'auth0|5d98fdc12424755c5b4c4095', 'Velediaz', 'Lily', '1990-01-01', '92020', '5', NULL,'I make iso apps for fun.', 'quin@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
+,('andres@fuckyou.com', 'auth0|5d98fdc12424650c5b4c4095', 'Torres', 'Jorge', '1990-01-01', '92020', '5', NULL,'A goalie looking for a team.', 'andres@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
+,('brandon@fuckyou.com', 'auth0|5d98fdc12427750c5b4c4095', 'Rodriguez', 'Will', '1990-01-01', '92020', '5', NULL,'New photographer looking to build portfolio', 'brandon@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
+,('louis@fuckyou.com', 'auth0|5d98fdc12424780c5b4c4095', 'Nguyen', 'Tristan', '1990-01-01', '92020', '5', NULL,'A model ready for shoots', 'louis@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
+,('harrison@fuckyou.com', 'auth0|5d98fdc12494750c5b4c4095', 'Pham', 'Yugi', '1990-01-01', '92020', '5', NULL,'Bouncer looking to start own club', 'harrison@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
+,('matt@fuckyou.com', 'auth0|5d98fdc12424758c5b4c4095', 'Xi', 'Seto', '1990-01-01', '92020', '5', NULL,'Fencer currently training for the olympics', 'matt@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43')
+,('raul@fuckyou.com', 'auth0|5d98fdc12424710c5b4c4095', 'Smith', 'John', '1990-01-01', '92020', '5', NULL,'Game consoles are life.', 'raul@fuckyou.com', NULL, 'active', '2019-10-12 00:02:44', '2019-10-12 02:39:43');
+
+USE project2DB;
+
+INSERT INTO matches (profileID1, profileID2, categoryType, matching, Success, createdAt, updatedAt)
+VALUES
+('auth0|5d98fdc1242475025b4c4095', 'auth0|5d98fdc1242475045b4c4095', 'Technology', true, true, '2019-10-12 00:02:44', '2019-10-12 02:39:43'),
+('auth0|5d98fdc12424755c5b4c4095', 'auth0|5d98fdc1242475045b4c4095', 'Technology',true,true,  '2019-10-12 00:02:44', '2019-10-12 02:39:43'),
+('auth0|5d98fdc12494750c5b4c4095', '', 'Nightlife',false,false,  '2019-10-12 00:02:44', '2019-10-12 02:39:43'),
+('auth0|5d98fdc1242475025b4c4095', 'auth0|5d98fdc12424755c5b4c4095', 'Technology',true,false,  '2019-10-12 00:02:44', '2019-10-12 02:39:43');
+
+USE project2DB;
+
+INSERT INTO chats( user, lastTime, message,createdAt, updatedAt, MatchId)
+VALUES
+('auth0|5d98fdc1242475045b4c4095','2019-10-12 00:02:44', 'Hi','2019-10-12 00:02:44', '2019-10-12 02:39:43', 1 ),
+('auth0|5d98fdc1242475045b4c4095','2019-10-12 00:02:44', 'Hi, how are you?','2019-10-12 00:02:44', '2019-10-12 02:39:43', 1 );
